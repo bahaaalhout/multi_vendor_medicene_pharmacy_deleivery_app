3bc6919e2a8f24e51db47ce367a503143ceefd17
bahaaalhout <bahaaalhout@gmail.com>
2026-01-06 14:12:45 +0200

    Add section bar to home screen

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,78 +1,80 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class AppTextStyles {
   // Header Text Styles
-  static const TextStyle header1 = TextStyle(
+  static TextStyle header1 = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 64,
+    fontSize: 64.sp,
     letterSpacing: -2,
   );
-  static const TextStyle header2 = TextStyle(
+  static TextStyle header2 = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 40,
+    fontSize: 40.sp,
     letterSpacing: -2,
   );
 
-  static const TextStyle header3 = TextStyle(
+  static TextStyle header3 = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 24,
+    fontSize: 24.sp,
     letterSpacing: -2,
   );
 
   // Subtitle/Body Large
-  static const TextStyle subtitle = TextStyle(
+  static TextStyle subtitle = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.w500,
-    fontSize: 24,
+    fontSize: 24.sp,
   );
 
   // Body Text Style
-  static const TextStyle body = TextStyle(
+  static TextStyle body = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.w400,
-    fontSize: 16,
+    fontSize: 16.sp,
     height: 1.4,
   );
 
   // Bold Text Style
-  static const TextStyle bold = TextStyle(
+  static TextStyle bold = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 16,
+    fontSize: 16.sp,
     height: 1.4,
   );
 
   // Small Text Style
-  static const TextStyle small = TextStyle(
+  static TextStyle small = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.w400,
-    fontSize: 14,
+    fontSize: 14.sp,
   );
 
   // Pre Title Text Style
-  static const TextStyle preTitle = TextStyle(
+  static TextStyle preTitle = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 10,
+    fontSize: 10.sp,
     letterSpacing: 3,
   );
 
   // Button Text Style
-  static const TextStyle buttonText = TextStyle(
+  static TextStyle buttonText = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 10,
+    fontSize: 10.sp,
     letterSpacing: 3,
   );
 
   // Link Text Style
-  static const TextStyle linkText = TextStyle(
+  static TextStyle linkText = TextStyle(
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.bold,
-    fontSize: 16,
+    fontSize: 16.sp,
     decoration: TextDecoration.underline,
   );
 }
