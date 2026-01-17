4f6b4a97fad33c652b8f64cdde24a64d08033795
AhmadAbd2020 <ahmadabdhaleem@gmail.com>
2026-01-17 13:28:13 +0200

    Home Screen

--- lib/features/patient/home/widgets/sales_item.dart
+++ lib/features/patient/home/widgets/sales_item.dart
@@ -10,59 +10,85 @@ class SalesItem extends StatelessWidget {
   final SalesModel salesModel;
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 36.w),
-          decoration: BoxDecoration(
-            color: AppColors.primaryLight,
-            borderRadius: BorderRadius.circular(12.r),
-          ),
-          child: Image.asset(salesModel.imageUrl, width: 100.w, height: 100.h),
-        ),
-        SizedBox(height: 8.h),
-
-        SizedBox(
-          width: 170.w,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                salesModel.name,
-                style: AppTextStyles.bold14.copyWith(
-                  color: AppColors.primaryDarker,
-                ),
-              ),
-              Icon(
-                Icons.add_circle_outline,
-                color: AppColors.primaryNormal,
-                size: 20.r,
-              ),
-            ],
-          ),
-        ),
-        SizedBox(height: 8.h),
-        TagsOfMedicine(salesModel: salesModel),
-        SizedBox(height: 8.h),
-        Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(
-              "\$${salesModel.discountedPrice}",
-              style: AppTextStyles.semiBold16.copyWith(
-                color: AppColors.successDark,
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 36.w),
+              decoration: BoxDecoration(
+                color: AppColors.primaryLight,
+                borderRadius: BorderRadius.circular(12.r),
+              ),
+              child: Image.asset(
+                salesModel.imageUrl,
+                width: 100.w,
+                height: 100.h,
               ),
             ),
-            SizedBox(width: 8.w),
-            Text(
-              "\$${salesModel.originalPrice}",
-              style: AppTextStyles.medium10.copyWith(
-                color: AppColors.neutralDark,
-                decoration: TextDecoration.lineThrough,
+            SizedBox(height: 8.h),
+
+            SizedBox(
+              width: 170.w,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    salesModel.name,
+                    style: AppTextStyles.bold14.copyWith(
+                      color: AppColors.primaryDarker,
+                    ),
+                  ),
+                  Icon(
+                    Icons.add_circle_outline,
+                    color: AppColors.primaryNormal,
+                    size: 20.r,
+                  ),
+                ],
               ),
+            ),
+            SizedBox(height: 8.h),
+            TagsOfMedicine(salesModel: salesModel),
+            SizedBox(height: 8.h),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  "\$${salesModel.discountedPrice}",
+                  style: AppTextStyles.semiBold16.copyWith(
+                    color: AppColors.successDark,
+                  ),
+                ),
+                SizedBox(width: 8.w),
+                Text(
+                  "\$${salesModel.originalPrice}",
+                  style: AppTextStyles.medium10.copyWith(
+                    color: AppColors.neutralDark,
+                    decoration: TextDecoration.lineThrough,
+                  ),
+                ),
+              ],
             ),
           ],
+        ),
+        Align(
+          alignment: Alignment.topLeft,
+
+          child: Container(
+            margin: EdgeInsets.only(left: 4.w, top: 4.h),
+            padding: EdgeInsets.symmetric(horizontal: 8.w, vertical: 4.h),
+            decoration: BoxDecoration(
+              color: AppColors.successDark,
+              borderRadius: BorderRadius.all(Radius.circular(8.r)),
+            ),
+            child: Text(
+              "${salesModel.discountPercentage}% OFF",
+              style: AppTextStyles.medium10.copyWith(
+                color: AppColors.successLight,
+              ),
+            ),
+          ),
         ),
       ],
     );
