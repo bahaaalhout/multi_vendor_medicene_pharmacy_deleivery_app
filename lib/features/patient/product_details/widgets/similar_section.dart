8b34e2152d4741f3031b6edfecfcb852b17af93b
AhmadAbd2020 <ahmadabdhaleem@gmail.com>
2026-01-19 20:57:57 +0200

    add similar section

---
+++ lib/features/patient/product_details/widgets/similar_section.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/home/models/sales_model.dart';
+import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/home/widgets/sales_item.dart';
+import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/home/widgets/title_of_headers.dart';
+
+class SimilarSection extends StatelessWidget {
+  const SimilarSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TitleOfHeaders(title: 'Similar', navigateToPage: () {}),
+        SizedBox(height: 12),
+        SizedBox(
+          height: 200.h,
+          child: ListView.separated(
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (context, index) => SizedBox(width: 10.w),
+            itemCount: sales.length,
+            itemBuilder: (context, index) {
+              return SalesItem(salesModel: sales[index]);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
