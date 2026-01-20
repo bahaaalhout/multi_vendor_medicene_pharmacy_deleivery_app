86cee62581b5e88ca98d1901f2e7c1ab21dda8a6
im7md33 <mohammed.abuawwad@fatoorah.app>
2026-01-20 15:07:20 +0200

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:multi_vendor_medicene_pharmacy_deleivery_app/core/constants/app_colors.dart';
+
 import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/navigation/patient_main_screen.dart';
 
 void main() {
