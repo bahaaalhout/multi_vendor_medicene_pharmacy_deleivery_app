54f33f3e482313b5a15858256e160a073899a3e0
im7md33 <mohammed.abuawwad@fatoorah.app>
2026-02-01 16:33:38 +0200

    help screens V2

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/help/screens/location_access_screen.dart';
 import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/notifications/view/notifications_page.dart';
 import 'package:multi_vendor_medicene_pharmacy_deleivery_app/features/patient/reminder/view/add_reminder_page.dart';
 import '../features/patient/navigation/patient_main_screen.dart';
@@ -10,7 +11,7 @@ class AppPages {
     routes: [
       GoRoute(
         path: AppRoutes.home,
-        builder: (context, state) => const MainScreen(),
+        builder: (context, state) => const LocationAccessScreen(),
       ),
       GoRoute(
         path: AppRoutes.notifications,
